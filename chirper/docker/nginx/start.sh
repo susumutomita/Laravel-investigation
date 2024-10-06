965f656596e2bbc21d36a033c44c01ab2ff8bb77
susumu tomita <oyster880@gmail.com>
2024-10-07 07:22:52 +0900

    add localtest

--- chirper/docker/nginx/start.sh
+++ chirper/docker/nginx/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# MySQLが起動するまで待機
+until mysql -h "mysql" -u "root" -p"password" -e 'select 1'; do
+  >&2 echo "MySQLがまだ起動していません。再試行中..."
+  sleep 3
+done
+
+# # マイグレーションとシーディングを実行
+php artisan migrate --force
+php artisan db:seed --force
+
 # Nginxをバックグラウンドで起動
 service nginx start
 
